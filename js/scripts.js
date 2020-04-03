2bbf73574616bd77c018b8333298a545f50b5bd9
Ethan Firpo <efirpo@gmail.com>
2020-04-03 11:54:39 -0700

    add image to html and styles, formats page

--- js/scripts.js
+++ js/scripts.js
@@ -6,31 +6,29 @@ function robotoGenerator(number) {
   var numberLine = [];
   var results = [];
 
-  // creates a array of strings from 0 -> user's inputted number.
+  // creates a array of strings from "0" --> user's inputted number.
 
   for (i = 0; i <= number; i++) {
-
-    numberLine.push(i.toString());
-    
+    numberLine.push(i.toString());  
   }
   console.log(numberLine);
+  
     // examines strings in numberLine and adds "beep," "boop," and "won't you be my neighbor?" + remaining numbers to results array, based on string values.
 
-    
-    for (i = 0; i <= number; i++) {
-      if ( numberLine[i].includes("3") ) {
-        results.push("won't you be my neighbor?")
-      }
-      else if ( numberLine[i].includes("2") ) {
-        results.push("boop")
-      }
-      else if ( numberLine[i].includes("1") ) {
-       results.push("beep") 
-      }
-      else { results.push(i.toString()) 
-      }
+  for (i = 0; i <= number; i++) {
+    if ( numberLine[i].includes("3") ) {
+      results.push("won't you be my neighbor?")
     }
-     console.log(results); 
+    else if ( numberLine[i].includes("2") ) {
+      results.push("boop")
+    }
+    else if ( numberLine[i].includes("1") ) {
+      results.push("beep") 
+    }
+    else { results.push(i.toString()) 
+    }
+  }
+    console.log(results); 
  }
 
 
@@ -39,7 +37,8 @@ function robotoGenerator(number) {
  $(document).ready(function() {
     $("form#numberForm").submit(function(event) {
       event.preventDefault();
-      var number = $("input#userInput").val();
+      var number = $("input#userNumber").val();
+      var name = $("input#userName").val();
       var output = robotoGenerator(number);
       $("#computed").show(output);
       console.log(number);
