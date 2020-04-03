b3bf07d4dc48559debf4bf4569317e974323b742
Ethan Firpo <efirpo@gmail.com>
2020-04-03 12:56:39 -0700

    attempt to add user-inputted name to final results of function

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,23 @@
 
 // function takes user's input and generates a list where numbers containg "1," "2," and "3," will be replaced with determined expected outputs of "beep," "boop," and "won't you be my neighbor?"
 
-function robotoGenerator(number) {
+function robotoGenerator(name, number) {
   var numberLine = [];
   var results = [];
-
+  var name = $("input#userName");
   // creates a array of strings from "0" --> user's inputted number.
 
   for (i = 0; i <= number; i++) {
     numberLine.push(i.toString());  
   }
   console.log(numberLine);
-  
+  console.log(name);
+
     // examines strings in numberLine and adds "beep," "boop," and "won't you be my neighbor?" + remaining numbers to results array, based on string values.
 
   for (i = 0; i <= number; i++) {
     if ( numberLine[i].includes("3") ) {
-      results.push("won't you be my neighbor?")
+      results.push("won't you be my neighbor " + name.toString() + "?")
     }
     else if ( numberLine[i].includes("2") ) {
       results.push("boop")
@@ -39,7 +40,7 @@ function robotoGenerator(number) {
       event.preventDefault();
       var number = $("input#userNumber").val();
       var name = $("input#userName").val();
-      var output = robotoGenerator(number);
+      var output = robotoGenerator(name, number);
       $("#computed").show(output);
       console.log(number);
     });
